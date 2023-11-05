cf75ba2f806024e61432a7d5d9a74f4a5b87bc3f
Killian RAIMBAUD <killian.rai@gmail.com>
2023-11-05 09:48:35 +0100

    Code format

--- NetworkHexEditor.py
+++ NetworkHexEditor.py
@@ -16,44 +16,44 @@ class NetworkHexEditor(Hexeditor):
         self.fileURL = url
 
         # Load data
-        self.network_manager = QNetworkAccessManager()
-        self.network_manager.finished.connect(self.handle_response)
+        self.networkManager = QNetworkAccessManager()
+        self.networkManager.finished.connect(self.handleResponse)
 
         # Additional Widgets
-        self.header_table = QTableWidget()
-        self.header_table.setColumnCount(2)
-        self.header_table.setHorizontalHeaderLabels(["Header", "Value"])
-        self.header_table.setColumnWidth(0, 150)
-        self.header_table.setColumnWidth(1, 300)
+        self.headerData = QTableWidget()
+        self.headerData.setColumnCount(2)
+        self.headerData.setHorizontalHeaderLabels(["Header", "Value"])
+        self.headerData.setColumnWidth(0, 150)
+        self.headerData.setColumnWidth(1, 300)
 
         self.requestInfo = QPlainTextEdit()
         self.requestInfo.setTextInteractionFlags(Qt.NoTextInteraction)
 
-        self.miscTabs.addTab(self.header_table, "HTTP Header")
+        self.miscTabs.addTab(self.headerData, "HTTP Header")
         self.miscTabs.addTab(self.requestInfo, "Request Info")
 
-        self.send_request(url)
+        self.sendRequest(url)
 
 
-    def send_request(self, url: str):
-        self.header_table.setRowCount(0)
+    def sendRequest(self, url: str):
+        self.headerData.setRowCount(0)
         self.requestInfo.clear()
         request = QNetworkRequest(QUrl(url))
-        self.network_manager.get(request)
+        self.networkManager.get(request)
 
-    def handle_response(self, reply):
+    def handleResponse(self, reply):
         if reply.error() == QNetworkReply.NoError:
-            status_code = reply.attribute(QNetworkRequest.HttpStatusCodeAttribute)
-            self.requestInfo.appendPlainText(f"HTTP Status Code: {status_code}")
+            statusCode = reply.attribute(QNetworkRequest.HttpStatusCodeAttribute)
+            self.requestInfo.appendPlainText(f"HTTP Status Code: {statusCode}")
 
             headers = reply.rawHeaderList()
             for header in headers:
-                header_cell = QTableWidgetItem(header.data().decode("utf-8"))
-                value_cell = QTableWidgetItem(reply.rawHeader(header).data().decode("utf-8"))
+                headerCell = QTableWidgetItem(header.data().decode("utf-8"))
+                valueCell = QTableWidgetItem(reply.rawHeader(header).data().decode("utf-8"))
 
-                self.header_table.insertRow(self.header_table.rowCount())
-                self.header_table.setItem(self.header_table.rowCount() - 1, 0, header_cell)
-                self.header_table.setItem(self.header_table.rowCount() - 1, 1, value_cell)
+                self.headerData.insertRow(self.headerData.rowCount())
+                self.headerData.setItem(self.headerData.rowCount() - 1, 0, headerCell)
+                self.headerData.setItem(self.headerData.rowCount() - 1, 1, valueCell)
 
             content = reply.readAll().data()
             self.setEditorData([b for b in content])
